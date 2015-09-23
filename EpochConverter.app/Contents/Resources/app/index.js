1e8410f330d20d2b08aabdb024d4c82f9ba1f0c5
KuroTsubasa1 <1-IBI@web.de>
2015-09-23 13:55:54 +0200

    Version 1.3.1
    fixed Copy Paste

--- EpochConverter.app/Contents/Resources/app/index.js
+++ EpochConverter.app/Contents/Resources/app/index.js
@@ -1,4 +1,6 @@
 var app = require('app');
+var clipboard = require('clipboard');
+var Menu = require("menu");
 var BrowserWindow = require('browser-window');
 
 var mainWindow = null;
@@ -13,5 +15,23 @@ app.on('ready', function(){
   mainWindow.loadUrl('file://' + __dirname + '/index.html');
 },5000);
 
-
+var template = [{
+        label: "Application",
+        submenu: [
+            { label: "About Application", selector: "orderFrontStandardAboutPanel:" },
+            { type: "separator" },
+            { label: "Quit", accelerator: "Command+Q", click: function() { app.quit(); }}
+        ]}, {
+        label: "Edit",
+        submenu: [
+            { label: "Undo", accelerator: "Command+Z", selector: "undo:" },
+            { label: "Redo", accelerator: "Shift+Command+Z", selector: "redo:" },
+            { type: "separator" },
+            { label: "Cut", accelerator: "Command+X", selector: "cut:" },
+            { label: "Copy", accelerator: "Command+C", selector: "copy:" },
+            { label: "Paste", accelerator: "Command+V", selector: "paste:" },
+            { label: "Select All", accelerator: "Command+A", selector: "selectAll:" }
+        ]}
+    ];
+    Menu.setApplicationMenu(Menu.buildFromTemplate(template));
 });
